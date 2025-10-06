cdb4bd42e3127462863087f0a8f1a224ab92eecf
Mayank Singh <dev.by.mayank@gmail.com>
2025-10-06 11:35:17 +0530

    udpated orders schema.

--- models/order.models.js
+++ models/order.models.js
@@ -1,17 +1,32 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
   products: [
     {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "products",
-        required: true,
+      type: {
+        title: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+        discountPercent: { type: Number, required: true },
+        discountPrice: { type: Number, required: true },
       },
-      quantity: { type: Number, min: 1, default: 1, required: true },
+      required: true,
     },
   ],
+  shippingAddress: {
+    type: {
+      _id: false,
+      name: { type: String, required: true },
+      streetAddress: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    required: true,
+  },
+  subTotal: { type: Number, min: 0, default: 0, required: true },
+  delivery: { type: Number, min: 0, default: 0, required: true },
+  totalToPay: { type: Number, min: 0, default: 0, required: true },
 });
 
 const Order = mongoose.model("orders", orderSchema);
