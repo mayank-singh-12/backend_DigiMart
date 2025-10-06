b033ae1c8eb874015f807ae462f07b9c551b7132
Mayank Singh <dev.by.mayank@gmail.com>
2025-10-06 11:37:31 +0530

    updated order schema

--- models/order.models.js
+++ models/order.models.js
@@ -4,6 +4,7 @@ const orderSchema = new mongoose.Schema({
   products: [
     {
       type: {
+        _id: false,
         title: { type: String, required: true },
         quantity: { type: Number, required: true },
         price: { type: Number, required: true },
